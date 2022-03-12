9a228c5159529875b6ceb3f5d6b657da031e442c
kadirkara22 <kadirkar2204@gmail.com>
2022-03-12 19:37:59 +0300

    ejs modulü indirilip,linkler düzenlendi

--- app.js
+++ app.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const app = express();
 
+app.set("view engine", "ejs");
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    const blog = { id: 1, title: "Blog title", description: "Blog description" }
-    res.send(blog)
+    res.render('index')
+})
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+app.get('/add_post', (req, res) => {
+    res.render('add_post')
 })
 
-const port = 3000;
+
+const port = 5000;
 
 app.listen(port, () => {
     console.log(`Sunucu ${port} portunda başlatıldı...`)
