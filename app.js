a2af9944621ebdfeb5f832eabf482bd8b73296fb
kadirkara22 <kadirkar2204@gmail.com>
2022-03-13 23:35:32 +0300

    mongoose ile gerekl bağlantı bilgileri kullanılarak bloglar post edildi

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const express = require('express');
+const ejs = require('ejs');
+const path = require('path')
+const Post = require('./modals/Post');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/cleanblog-test-db')
 
 app.set("view engine", "ejs");
 app.use(express.static('public'))
 
-app.get('/', (req, res) => {
-    res.render('index')
+app.get('/', async (req, res) => {
+    const posts = await Post.find({})
+    res.render('index', {
+        posts
+    })
 })
 app.get('/about', (req, res) => {
     res.render('about')
 })
 app.get('/add_post', (req, res) => {
     res.render('add_post')
+})
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.post('/posts', async (req, res) => {
+    await Post.create(req.body)
+    res.redirect('/')
 })
 
 
